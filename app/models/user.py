76bf474168ddad3791caa2acbc8d82391a892d1b
Araceli <aracelimaldonado4@yahoo.com>
2021-08-17 01:44:23 -0700

    updated user model

--- app/models/user.py
+++ app/models/user.py
@@ -2,14 +2,16 @@ from flask_login import UserMixin
 from app import db
 
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     birthday = db.Column(db.String)
+    about_me = db.Column(db.String)
     username = db.Column(db.String, nullable=False, unique=True)
     # email = db.Column(db.String)
-    password = db.Column(db.String, nullable=False)
+    password1 = db.Column(db.String, nullable=False)
+    password2 = db.Column(db.String)
 
     def to_json_user(self):
         return {
@@ -17,9 +19,10 @@ class User(db.Model, UserMixin):
             "first name": self.first_name,
             "last name": self.last_name,
             "birthday": self.birthday,
+            "about me": self.about_me,
             "username": self.username,
             # "email": self.email,
-            "password": self.password
+            "password": self.password1
         }
 
 
--- app/routes.py
+++ app/routes.py
@@ -36,25 +36,26 @@ def register():
 
     request_body = request.get_json()
 
-    if request_body['first name'] is '' or request_body['last name'] is '' or request_body['birthday'] is '' or request_body['username'] is '' or request_body['password'] is '':
+    if request_body['first name'] == '' or request_body['last name'] == '' or request_body['birthday'] == '' or request_body['about me'] == '' or request_body['username'] == '' or request_body['password1'] == '' or request_body['password2'] == '':
         return jsonify({'Error': 'Enter info for all fields.'}), 400
-
+    
     first_name = request_body['first name']
     last_name = request_body['last name']
     birthday = request_body['birthday']
+    about_me = request_body['about me']
     username = request_body['username'].lower()
     # email = request_body['email']
-    password1 = request_body['password']
-    # password2 = request_body['password2']
+    password1 = request_body['password1']
+    password2 = request_body['password2']
 
-    # if password1 != password2: 
-    #     return jsonify({'Error': 'Passowrds do not match'}), 400
+    if password1 != password2: 
+        return jsonify({'Error': 'Passwords do not match'}), 400
 
 
     username_exists = User.query.filter_by(username=username).first()
 
     if username_exists is None: 
-        new_user = User(first_name=first_name, last_name=last_name, birthday=birthday, username=username, password=generate_password_hash(password1, method='sha256'))
+        new_user = User(first_name=first_name, last_name=last_name, birthday=birthday, about_me=about_me, username=username, password1=generate_password_hash(password1, method='sha256'))
         db.session.add(new_user)
         db.session.commit()
 
@@ -66,6 +67,7 @@ def register():
 
 @users_bp.route('/users/<user_id>', methods = ['GET'])
 def handle_user(user_id):
+    
 
     user = User.query.get(user_id)
 
@@ -103,21 +105,21 @@ def delete_all_users():
 
     return jsonify({'Success': 'All users deleted'}), 200
 
-@users_bp.route('/login', methods = ['GET', 'POST'])
+@users_bp.route('/login', methods = ['POST'])
 def login():
     d = {}
 
     request_body = request.get_json()
 
     username = request_body['username']
-    password = request_body['password']
+    password = request_body['password1']
 
     user = User.query.filter_by(username=username).first()
 
     if user is None: 
         return jsonify({'Error': 'Username does not exist'}), 400
     else:
-        if check_password_hash(user.password, password):
+        if check_password_hash(user.password1, password):
             # login_user(user, remember=True)
             return jsonify({'Success': 'You are now logged in'}), 200
         else:
